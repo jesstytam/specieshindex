157d0f7fd329197b6dd4b55a62fa5922a3799577
jessicatytam <z5093095@student.unsw.edu.au>
2021-03-09 15:26:03 +1100

    updated documentation

---
+++ R/languages.R
@@ -0,0 +1,9 @@
+#' List of languages of documents found on Scopus.
+#' Data was retrieved on 9 March 2021.
+#' 
+#' @title Languages
+#'
+#' @format A csv file with the complete list of languages of documents found on Scopus.
+#'
+#' @source \url{https://www.elsevier.com/solutions/scopus/how-scopus-works/content}
+"languages"
