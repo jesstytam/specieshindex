c6ea39fabd17131e19eb324344408b63b3ba76d1
jessicatytam <z5093095@student.unsw.edu.au>
2021-04-27 15:35:09 +1000

    forgot to add to both T and TAK, trying again

--- R/specieshindex.R
+++ R/specieshindex.R
@@ -551,6 +551,7 @@ CountSpTAK_wos <- function(genus, species, synonyms, additionalkeywords) {
 #' @param additionalkeywords Optional search terms.
 #'
 #' @return A dataframe of the species' citation records with the given \code{genus} and \code{species}.
+#' @importFrom data.table rbindlist setDT .SD
 #' @export
 #'
 #' @examples
@@ -575,8 +576,8 @@ FetchSpT_wos <- function(genus, species, synonyms, additionalkeywords) {
   )
   query <- wosr::pull_wos(query = create_query_string_T_wos(genus, species, synonyms, additionalkeywords),
                           sid = sid) 
-  results <- data.table::rbindlist(query, fill = TRUE)
-  results <- data.table::setDT(results)[, lapply(data.table:::.SD, function(x) toString(na.omit(x))), by = ut]
+  results <- rbindlist(query, fill = TRUE)
+  results <- setDT(results)[, lapply(.SD, function(x) toString(na.omit(x))), by = ut]
   #renaming columns
   names(results)[names(results) == "tot_cites"] <- "citations"
   names(results)[names(results) == "doc_type"] <- "description"
@@ -625,8 +626,8 @@ FetchSpTAK_wos <- function(genus, species, synonyms, additionalkeywords) {
   )
   query <- wosr::pull_wos(query = create_query_string_TAK_wos(genus, species, synonyms, additionalkeywords),
                           sid = sid) 
-  results <- data.table::rbindlist(query, fill = TRUE)
-  results <- data.table::setDT(results)[, lapply(data.table:::.SD, function(x) toString(na.omit(x))), by = ut]
+  results <- rbindlist(query, fill = TRUE)
+  results <- setDT(results)[, lapply(.SD, function(x) toString(na.omit(x))), by = ut]
   #renaming columns
   names(results)[names(results) == "tot_cites"] <- "citations"
   names(results)[names(results) == "doc_type"] <- "description"
