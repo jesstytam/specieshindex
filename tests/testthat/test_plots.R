2f37e2a35deebd83a050ac64d8bad4ce4f1d14b4
jessicatytam <z5093095@student.unsw.edu.au>
2021-11-09 14:52:27 +1100

    fixed testing_plots

--- tests/testthat/test_plots.R
+++ tests/testthat/test_plots.R
@@ -11,8 +11,13 @@ test_that("preparing_data_for_plots", {
 })
 
 test_that("plotting", {
+  B_penicillata <- Allindices(Woylie, "Bettongia", "penicillata")
+  index_plot <- plotAllindices(B_penicillata)
   expect_identical(class(index_plot$layers[[1]]$geom)[1], "GeomDrawGrob")
   expect_identical(class(index_plot$layers[[1]]$stat)[1], "StatIdentity")
+  Woylie_year <- getYear(Woylie, "Bettongia", "penicillata")
+  pub_plot <- plotPub(Woylie_year)
   expect_identical(class(pub_plot$layers[[1]]$geom)[1], "GeomLine")
   expect_identical(class(pub_plot$layers[[1]]$stat)[1], "StatIdentity")
 })
+
